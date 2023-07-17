1ee02a6f852b37e4b7702b89c2abbe061657ec3a
Jun Yabe <yabe@phoenix.local>
2023-07-17 09:54:06 +0900

    Added xmloutputter

--- main.cpp
+++ main.cpp
@@ -6,30 +6,36 @@
 #include <cppunit/TestRunner.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/ui/text/TestRunner.h>
-
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/TextOutputter.h>
 #include "http_client.h"
 #include "debug.h"
 
+#define TEST_OUTPUT_FILE "test_result.xml"
+
 int main(int argc, const char** argv)
 {
-CPPUNIT_NS::TestResult controller;
+	CPPUNIT_NS::TestResult controller;
 
-    //結果収集
-    CPPUNIT_NS::TestResultCollector result;
-    controller.addListener(&result);
+	//途中結果の収集
+	CPPUNIT_NS::BriefTestProgressListener progress;
+	controller.addListener(&progress);
 
-    //途中結果の収集
-    CPPUNIT_NS::BriefTestProgressListener progress;
-    controller.addListener(&progress);
+	//テストを走らせる。テストを入れて走る
+	CPPUNIT_NS::TestRunner runner;
+	runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
+ 
+	CppUnit::TestResultCollector result; 
+	controller.addListener ( &result ); 
+ 
+	std::ofstream xmlout (TEST_OUTPUT_FILE); 
+	CppUnit::XmlOutputter xmlOutputter ( &result, xmlout ); 
+	CppUnit::TextOutputter consoleOutputter ( &result, std::cout ); 
+	
+	runner.run(controller); 
+ 
+	xmlOutputter.write(); 
+	consoleOutputter.write(); 
 
-    //テストを走らせる。テストを入れて走る
-    CPPUNIT_NS::TestRunner runner;
-    runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
-    runner.run(controller);
-
-    //結果を標準出力にする。
-    CPPUNIT_NS::CompilerOutputter outputter(&result,CPPUNIT_NS::stdCOut());
-    outputter.write();
-
-    return result.wasSuccessful() ? 0 : 1;	
+	return result.wasSuccessful() ? 0 : 1;	
 }
